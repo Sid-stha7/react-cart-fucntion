f36b87283ee641db82003eb08ef6027029658b94
sid_stha7 <sidstha444@gmail.com>
2022-12-04 15:41:00 +0545

    single task prop and component

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import Header from "./components/header";
 import Tasks from "./components/Tasks";
 
-
 function App() {
-  
-  return (
-<div className="App">
-   
-    {/* passing props   */}
-    <div className="container">
-      <Header /> 
-    <Tasks/>
-      </div>
-    
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: "true",
+    },
 
-    {/* title is props  */}
-    
-   
+    {
+      id: 2,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: "true",
+    },
+
+    {
+      id: 3,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: "false",
+    },
+  ]);
+  return (
+    <div className="App">
+      {/* passing props   */}
+      <div className="container">
+        <Header />
+        <Tasks tasks={tasks} />
+      </div>
+
+      {/* title is props  */}
     </div>
   );
 }
